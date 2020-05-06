b915023687b79f98b9df840e52f61a07c870469d
chunning-tsao <chunning.tsao@gmail.com>
2020-05-05 21:03:56 -0700

    add break for multi-lines expression

--- pytorch-CycleGAN-and-pix2pix/models/cycle_gan_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/cycle_gan_model.py
@@ -221,7 +221,7 @@ class CycleGANModel(BaseModel):
         self.loss_temp_rec_B = self.criterionTemp(self.rec_B[:-1], self.warp_rec_B[1:], self.confidences[1:]).detach()
 
         # combined loss and calculate gradients
-        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B
+        self.loss_G = self.loss_G_A + self.loss_G_B + self.loss_cycle_A + self.loss_cycle_B + self.loss_idt_A + self.loss_idt_B\
                     + self.loss_temp_fake_A + self.loss_temp_rec_B
         self.loss_G.backward()
 
